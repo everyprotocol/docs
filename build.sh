2a38d647961c9ed28177895c703b1ea3d853ce53
Brice <briceyan3.0@gmail.com>
2025-05-15 20:16:31 +0800

    refactor: move contract docs from v1-core to core directory

--- build.sh
+++ build.sh
@@ -13,27 +13,27 @@ repo=repos/periphery
 for name in SetRegistry KindRegistry ObjectMinter \
     RelationRegistry ObjectInteraction ObjectAuthorization ValueRegistry UniqueRegistry ; do
   tail -n +4 "$repo/docs/src/contracts/interfaces/core/I${name}.sol/interface.I${name}.md" \
-    > "docs/contracts/v1-core/${name}.mdx"
+    > "docs/contracts/core/${name}.mdx"
 done
 
 name=OmniRegistry
-file="docs/contracts/v1-core/${name}.mdx"
+file="docs/contracts/core/${name}.mdx"
 cat > $file <<EOF
 **Inherits:**
-[RelationRegistry](/contracts/v1-core/RelationRegistry), [ObjectAuthorization](/contracts/v1-core/ObjectAuthorization), [ObjectInteraction](/contracts/v1-core/ObjectInteraction)
+[RelationRegistry](/contracts/core/RelationRegistry), [ObjectAuthorization](/contracts/core/ObjectAuthorization), [ObjectInteraction](/contracts/core/ObjectInteraction)
 EOF
 tail -n +6 "$repo/docs/src/contracts/interfaces/core/I${name}.sol/interface.I${name}.md" >> $file
 
 name=ElementRegistry
-file="docs/contracts/v1-core/${name}.mdx"
+file="docs/contracts/core/${name}.mdx"
 cat > $file <<EOF
 **Inherits:**
-[ValueRegistry](/contracts/v1-core/ValueRegistry), [UniqueRegistry](/contracts/v1-core/UniqueRegistry)
+[ValueRegistry](/contracts/core/ValueRegistry), [UniqueRegistry](/contracts/core/UniqueRegistry)
 EOF
 tail -n +6 "$repo/docs/src/contracts/interfaces/core/I${name}.sol/interface.I${name}.md" >> $file
 
 
 for name in ISet IInteroperable IRemoteMintable ISetRegistryAdmin IObjectMinterAdmin; do
   tail -n +4 "$repo/docs/src/contracts/interfaces/user/${name}.sol/interface.${name}.md" \
-    > "docs/contracts/v1-periphery/${name}.mdx"
+    > "docs/contracts/periphery/${name}.mdx"
 done

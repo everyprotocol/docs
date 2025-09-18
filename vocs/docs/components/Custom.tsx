969011509e31d563e05f047bfd0e7f683089e53d
Brice <briceyan3.0@gmail.com>
2025-09-18 15:28:47 +0800

    docs: refine wording in guides

--- vocs/docs/components/Custom.tsx
+++ vocs/docs/components/Custom.tsx
@@ -21,7 +21,7 @@ export function Feature({ title, desc }: FeatureProps) {
   return (
     <div className="flex gap-2 text-left flex-col items-start border feature rounded-sm py-2 px-2 prose prose-neutral dark:prose-invert">
       <h3 className="font-medium">{title}</h3>
-      <p className="desc text-sm">{desc}</p>
+      <p className="desc text-md">{desc}</p>
     </div>
   );
 }
